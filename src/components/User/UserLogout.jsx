02ba23d0d0e8baec1b437e7ea71ad1693a09cadb
BintangAull <bintangokelah@gmail.com>
2025-06-07 14:25:38 +0700

    menyesuaikan vibe seluruh halaman nya color palette dan lain lain bikin intercepter manual karena aku ga ngerti axios ya sederhana la yah jadi kalian tinggal pake aja

---
+++ src/components/User/UserLogout.jsx
@@ -0,0 +1,65 @@
+import {useEffectOnce, useLocalStorage} from "react-use";
+import checkAndRefreshToken from "../../lib/CheckToken.js";
+import {alertError, alertSuccess} from "../../lib/alert.js";
+import {useNavigate} from "react-router";
+import {userLogout} from "../../lib/api/UserApi.jsx";
+
+
+
+
+
+export default function UserLogout() {
+
+    const [token, setToken] = useLocalStorage('access_token', '');
+    const [refresh_token, setRefresh_token] = useLocalStorage('refresh_token', '');
+    const navigate = useNavigate();
+
+
+    async function handleLogout() {
+        //cek refresh token bro
+        const tokenStatus = await checkAndRefreshToken(token, refresh_token);
+        // refresh token nya basi
+        //login ulang
+        if(tokenStatus.needsLogin){
+            await alertError("Token expired, please login again");
+            console.log(tokenStatus);
+            navigate({
+                pathname: "/login",
+            })
+            return;
+        }
+        // Kalo si acces token nya basi
+        if (tokenStatus.isValid){
+            //kita arahin supaya ganti token
+            if (tokenStatus.token !== token){
+                setToken(tokenStatus.token);
+            }
+        }
+
+        //barulah kita otak atik disini
+        const response = await userLogout(token, {refresh_token});
+        if(response.status === 200){
+            await alertSuccess("Logout berhasil");
+            setToken('');
+            setRefresh_token('');
+            navigate({
+                pathname: "/login",
+            })
+
+        }else{
+            await alertError("something went wrong, please try again");
+        }
+
+    }
+
+
+    useEffectOnce(() => {
+        handleLogout()
+            .then(()=> console.log("sukses"))
+
+    })
+
+    return <>
+
+    </>
+}
